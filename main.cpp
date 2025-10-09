ab65c86eed36c58fc6e9c2e1a168b55df613fc8a
Mitchell Lojszczyk <a1968218@student.adelaide.edu.au>
2025-10-09 15:26:31 +1030

    just playing around lol

--- main.cpp
+++ main.cpp
@@ -27,9 +27,11 @@ int main()
     CountdownText* countdown = new CountdownText(250, 200, 72);
     int countdownTextID = engine.mountRenderable(countdown);//add countdown text to the render loop. store ID for removal at a later time
 
-    countdown->setDismountCallback([&engine, countdownTextID](CountdownText* obj) {//called after the time left reaches 0
+    countdown->setDismountCallback([&engine, countdownTextID, text](CountdownText* obj) {//called after the time left reaches 0
         engine.dismountPhysicsElement(countdownTextID);//remove text from scene
-        engine.clearAllRenderables(); // remove all items
+        text->setText("Woah");
+        text->setAnimationStyle(TextAnimation::None);
+        text->setColor(sf::Color::Yellow);
     });
 
 
